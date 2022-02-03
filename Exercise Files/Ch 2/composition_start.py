22d6f982d61583d66353b9ec5196b254ede2f0b6
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-02-03 16:24:42 +0700

    ch2

--- Exercise Files/Ch 2/composition_start.py
+++ Exercise Files/Ch 2/composition_start.py
@@ -2,24 +2,47 @@
 # Using composition to build complex objects
 
 
+from functools import reduce
+
+
 class Book:
-    def __init__(self, title, price, authorfname, authorlname):
+    def __init__(self, title, price, author=None):
         self.title = title
         self.price = price
 
-        self.authorfname = authorfname
-        self.authorlname = authorlname
+        self.author = author
 
         self.chapters = []
 
-    def addchapter(self, name, pages):
-        self.chapters.append((name, pages))
+    def addchapter(self, chapter):
+        self.chapters.append(chapter)
+
+    def getbookpagecount(self):
+        return reduce(lambda x, y: x + y, [i.pagecount for i in self.chapters])
 
 
-b1 = Book("War and Peace", 39.0, "Leo", "Tolstoy")
+class Author:
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
 
-b1.addchapter("Chapter 1", 125)
-b1.addchapter("Chapter 2", 97)
-b1.addchapter("Chapter 3", 143)
+    def __str__(self):
+        return f"{self.firstname} { self.lastname}"
 
+
+class Chapter:
+    def __init__(self, name, pagecount):
+        self.name = name
+        self.pagecount = pagecount
+
+
+auth = Author('Leo', 'tolstoy')
+b1 = Book("War and Peace", 39.0, auth)
+
+b1.addchapter(Chapter("Chapter 1", 125))
+b1.addchapter(Chapter("Chapter 2", 97))
+b1.addchapter(Chapter("Chapter 3", 143))
+
+print(b1.author)
 print(b1.title)
+print(b1.getbookpagecount())
--- Exercise Files/Ch 2/multiple_start.py
+++ Exercise Files/Ch 2/multiple_start.py
@@ -6,17 +6,26 @@ class A:
     def __init__(self):
         super().__init__()
         self.foo = "foo"
+        self.name = 'A'
 
 
 class B:
     def __init__(self):
         super().__init__()
         self.bar = "bar"
+        self.name = 'B'
 
 
+# class C(B, A):# try this
 class C(A, B):
     def __init__(self):
         super().__init__()
 
+    def showprops(self):
+        print(self.foo)
+        print(self.bar)
+        print(self.name)  # first inheritance
+
 
 c = C()
+c.showprops()
